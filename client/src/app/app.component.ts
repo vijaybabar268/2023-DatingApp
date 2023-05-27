aae01d341f92be131fbbfdee87c9b966b50e6743
vijaybabar <babarvm@gmail.com>
2023-05-27 14:09:51 +0530

    End of the section 5 part 10 - Parent to child communication

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -11,14 +11,12 @@ import { User } from './_models/User';
 export class AppComponent implements OnInit {
 
   title = 'Dating App';
-  users: any;
   user: any;
 
   constructor(private http: HttpClient, private accountService: AccountService){
   }
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
 
     this.http.get('https://localhost:5001/api/users/1').subscribe({
@@ -26,15 +24,7 @@ export class AppComponent implements OnInit {
       error: error => console.log(error),
       complete: () => console.log('Request has completed with userId.')
     })
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed.')
-    })
-  }
+  }  
   
   setCurrentUser() {
     const userString = localStorage.getItem("user");
