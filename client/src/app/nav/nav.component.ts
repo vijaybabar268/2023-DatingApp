f647a47846f43498ab5ef40cb0f0df084a88eae0
vijaybabar <babarvm@gmail.com>
2023-05-27 13:17:18 +0530

    End of the section 5 part 7 - Using the async pipe part 2

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -10,12 +10,10 @@ import { User } from '../_models/User';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  currentUser$: Observable<User | null> =  of(null);
 
-  constructor(private accountService: AccountService) { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.currentUser$ = this.accountService.currentUser$;
   }
 
   login() {
