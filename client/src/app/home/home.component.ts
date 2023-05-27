c234311c54ef47f55963cca1e28ce786738b0e0b
vijaybabar <babarvm@gmail.com>
2023-05-27 14:17:45 +0530

    End of the section 5 part 11 - Child to Parent communication

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -27,4 +27,8 @@ export class HomeComponent implements OnInit {
       complete: () => console.log('Request has completed.')
     })
   }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
 }
