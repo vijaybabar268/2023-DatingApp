0469c7dccd664125a600ad9f4e496a3337ddd9ce
vijaybabar <babarvm@gmail.com>
2023-06-07 23:53:41 +0530

    End of the section 8 part 1 - Extend entity & Added DateTime Extenstion method to calculate age

---
+++ API/Entities/photo.cs
@@ -0,0 +1,10 @@
+namespace API.Entities
+{
+    public class photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+        public string PublicId { get; set; }
+    }
+}
