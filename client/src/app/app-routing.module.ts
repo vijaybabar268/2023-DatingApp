8224a93315b3b8c9b759fb3ed212ea078a986ecb
vijaybabar <babarvm@gmail.com>
2023-06-07 22:14:20 +0530

    End of the section 7 part 7 - Handling not found

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { TestErrorComponent } from './errors/test-error/test-error.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
@@ -21,6 +23,8 @@ const routes: Routes = [
     ]
   },
   { path: 'errors', component:TestErrorComponent },
+  { path: 'not-found', component:NotFoundComponent },
+  { path: 'server-error', component:ServerErrorComponent },
   { path: '**', component: HomeComponent, pathMatch: 'full'},
 ];
 
