de5f99d6dd48c1620b30e9776a8d3874069cb4b6
vijaybabar <babarvm@gmail.com>
2023-06-09 00:33:30 +0530

    End of the section 8 part 9 - Configuring AutoMapper

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -8,7 +8,9 @@ namespace API.Helpers
     {
         public AutoMapperProfiles()
         {
-            CreateMap<AppUser, MemberDto>();
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(x => x.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url));
+
             CreateMap<photo, photoDto>();
         }
     }
