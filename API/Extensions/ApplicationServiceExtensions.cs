b0e24aeb4c5804be67000cb3142e0cf2e4c351f8
vijaybabar <babarvm@gmail.com>
2023-06-09 00:17:43 +0530

    End of the section 8 part 7 - Adding AutoMapper

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -23,7 +23,8 @@ namespace API.Extensions
             
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
-
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            
             return services;
         }
     }
---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+using API.Dtos;
+using API.Entities;
+using AutoMapper;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>();
+            CreateMap<photo, photoDto>();
+        }
+    }
+}
