45f1a83373638be01525c1b91f2529b1329834c8
vijaybabar <babarvm@gmail.com>
2023-06-24 00:54:00 +0530

    End of the section 11 part 7 - Setting the main photo image in Navbar

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -47,6 +47,7 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.Users
+                        .Include(p => p.Photos)
                         .SingleOrDefaultAsync(u => u.UserName == loginDto.Username);
 
             if (user == null) return Unauthorized("Invalid username.");   
@@ -62,7 +63,8 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
