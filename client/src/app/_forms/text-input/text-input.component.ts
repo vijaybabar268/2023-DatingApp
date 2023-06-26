89e1aecf242d5790666dbe952515e62f8e6fc303
vijaybabar <babarvm@gmail.com>
2023-06-26 23:28:16 +0530

    End of the section 12 part 2 - Creating a re-usable text input part 2

--- client/src/app/_forms/text-input/text-input.component.ts
+++ client/src/app/_forms/text-input/text-input.component.ts
@@ -15,15 +15,12 @@ export class TextInputComponent implements ControlValueAccessor {
   }
 
   writeValue(obj: any): void {
-    throw new Error('Method not implemented.');
   }
 
   registerOnChange(fn: any): void {
-    throw new Error('Method not implemented.');
   }
 
   registerOnTouched(fn: any): void {
-    throw new Error('Method not implemented.');
   }
 
   get control(): FormControl {
