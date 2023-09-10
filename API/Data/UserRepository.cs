eb140057713c3b28a4533acf2a9c975185aa1eeb
vijaybabar <babarvm@gmail.com>
2023-09-10 20:34:32 +0530

    End of the section 13 part 7 - Adding additional filters

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -72,6 +72,11 @@ namespace API.Data
 
             query = query.Where(u => u.UserName != userParams.CurrentUsername);
             query = query.Where(u => u.Gender == userParams.Gender);
+
+            var minDob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MaxAge - 1));
+            var maxBob = DateOnly.FromDateTime(DateTime.Today.AddYears(-userParams.MinAge));
+
+            query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxBob);
                         
             return await PagedList<MemberDto>.CreateAsync(
                 query.AsNoTracking().ProjectTo<MemberDto>(_mapper.ConfigurationProvider), 
--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -17,5 +17,8 @@
         public string CurrentUsername { get; set; }
         
         public string Gender { get; set; }
+
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 100;
     }
 }
