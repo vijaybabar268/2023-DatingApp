490144c1aa7ae3685a4894672e33bb482c7f91ae
vijaybabar <babarvm@gmail.com>
2023-09-10 17:05:51 +0530

    End of the section 13 part 2 - Adding helper classes for pagination

---
+++ API/Extensions/HttpExtensions.cs
@@ -0,0 +1,16 @@
+﻿using API.Helpers;
+using System.Text.Json;
+
+namespace API.Extensions
+{
+    public static class HttpExtensions
+    {
+        public static void AddPaginationHeader(this HttpResponse reponse, PaginationHeader header)
+        {
+            var jsonOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            reponse.Headers.Add("Pagination", JsonSerializer.Serialize(header, jsonOptions));
+            reponse.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+
+        }
+    }
+}
